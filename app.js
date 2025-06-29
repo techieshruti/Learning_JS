88b37c4371cea6a1f28fdcd0efea60e759d09917
techieshruti <shrutichandra721@gmail.com>
2025-06-29 12:53:28 +0530

    added heading and paragraph

---
+++ app.js
@@ -0,0 +1,20 @@
+// created a container and added it to the body!
+const container = document.createElement("div");
+document.body.appendChild(container);
+
+// Added a heading
+const heading1=document.createElement("h1");
+heading1.textContent="Manali";
+const heading2= document.createElement("h2");
+heading2.textContent="Town in Himachal Pradesh";
+container.appendChild(heading1);
+container.appendChild(heading2);
+
+// added a image
+// const image= document.createElement("img");
+// container.appendChild(image);
+
+// added a paragraph
+const para = document.createElement("p");
+para.textContent="Manali is a high-altitude Himalayan resort town in India’s northern Himachal Pradesh state. It has a reputation as a backpacking center and honeymoon destination. Set on the Beas River, it’s a gateway for skiing in the Solang Valley and trekking in Parvati Valley.";
+container.appendChild(para);
