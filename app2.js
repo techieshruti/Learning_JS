176a8fb965c0aaad8cff4af8b1160fb4c908033a
techieshruti <shrutichandra721@gmail.com>
2025-07-01 19:13:27 +0530

    for each loop

--- app2.js
+++ app2.js
@@ -3,3 +3,9 @@
 for(let i =1; i<=5; i++){
     console.log(i);
 }
+
+//🥈 Level 2: Greet all users in a list
+let users = ["Rahul", "Simran", "Amit"];
+users.forEach(function(greet){
+    console.log("Hello "+ greet);
+});
