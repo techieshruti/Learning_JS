63261f005ccf007e3360aa07458f2a3cb20e33c3
techieshruti <shrutichandra721@gmail.com>
2025-10-02 22:54:06 +0530

    added new script file

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ let b = 20;
 console.log(b); //20
 const c = 30;
 console.log(c); //30
+
+
+function multiply(a, b) {
+  var result = a * b;
+  return result;
+}
+
+var x = multiply(2, 3); 
+var y = multiply(4, 5);
+
+console.log(x); // 6
+console.log(y); // 20
