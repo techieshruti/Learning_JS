b17bc91eb2c9650beda7c84cd16d74df46647d86
techieshruti <shrutichandra721@gmail.com>
2025-11-13 12:43:47 +0530

    insert value While Maintaining Sorted Order

--- array.js
+++ array.js
@@ -268,25 +268,22 @@
 
 // 21. Insert Multiple Values at Once
 // Insert [60,70,80] at position 3 in [10,20,30,40,50] manually.
-let arr = [10, 20, 30, 40, 50];
-let k = [60, 70, 80];
+// let arr = [10, 20, 30, 40, 50];
+// let k = [60, 70, 80];
 
-function insertMultiple(arr, pos, k) {
-  let count = k.length;
-  // Step 1: Shift existing elements right by count
-  for (let i = arr.length - 1; i >= pos; i--) {
-    arr[i + count] = arr[i];
-  }
-  console.log(arr)
-  //Step 2: Insert all new elements
-  for (let j = 0; j < count; j++) {
-    arr[pos + j] = k[j];
-  }
-  return arr;
-}
-console.log(insertMultiple(arr, 3, k));
-
-
+// function insertMultiple(arr, pos, k) {
+//   let count = k.length;
+//   // Step 1: Shift existing elements right by count
+//   for (let i = arr.length - 1; i >= pos; i--) {
+//     arr[i + count] = arr[i];
+//   }
+//   //Step 2: Insert all new elements
+//   for (let j = 0; j < count; j++) {
+//     arr[pos + j] = k[j];
+//   }
+//   return arr;
+// }
+// console.log(insertMultiple(arr, 3, k));
 
 
 // 22. Remove Duplicates (without Set)
