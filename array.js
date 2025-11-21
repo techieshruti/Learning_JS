db8cf229509a52adfe5f7f616cc26f82445c6da1
techieshruti <shrutichandra721@gmail.com>
2025-11-21 17:10:01 +0530

    (LeetCode 35)

--- array.js
+++ array.js
@@ -539,3 +539,22 @@ var compress = function(chars) {
 
     return writeIdx; // The new length of the compressed array
 };
+
+//leetcode 35.
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid; // Target found, return its index
+        } else if (nums[mid] < target) {
+            left = mid + 1; // Target is in the right half
+        } else {
+            right = mid - 1; // Target is in the left half
+        }
+    }
+    return left;
+};
