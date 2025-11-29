187fc8360cc34eb6a39eeaca686655c9d6f2fed2
techieshruti <shrutichandra721@gmail.com>
2025-11-29 23:55:30 +0530

    a

--- array.js
+++ array.js
@@ -541,20 +541,3 @@ var compress = function(chars) {
 };
 
 //leetcode 35 Search Insert Position.
-var searchInsert = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
-            return mid; // Target found, return its index
-        } else if (nums[mid] < target) {
-            left = mid + 1; // Target is in the right half
-        } else {
-            right = mid - 1; // Target is in the left half
-        }
-    }
-    return left;
-};
